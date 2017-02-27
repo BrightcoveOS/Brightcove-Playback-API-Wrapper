e58a59168240731e62499929d9c8bbe9d4ddeff5
Theresa Newman <theresa.newman@hmhco.com>
2017-02-27 16:35:01 -0500

    remove license from code file

--- bc-papi.php
+++ bc-papi.php
@@ -11,25 +11,7 @@
  * get video by id - pass type=videos and video id to find method
  * get video by reference id - pass type=videos and reference id e.g ref:12345 to find method
  * get playlist by id - pass type=playlists and playlist id  to find method
- * playlist by reference id - pass type=playlists and reference id e.g ref:12345 to find method
- *
- *
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
- * and associated documentation files (the "Software"), to deal in the Software without restriction, 
- * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included 
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
- *NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
- THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
+ * playlist by reference id - pass type=playlists and reference id e.g ref:12345 to find method *
  *
 **/
 
